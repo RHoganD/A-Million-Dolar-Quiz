65e04438bff1e3c0bdc0094e80ade9bfc5d61168
RHoganD <richpatric@hotmail.com>
2022-10-19 16:11:44 +0000

    Add structure functions in the script js

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+function runGame(){
+
+}
+
+function QuestionGameArea(){
+
+}
+
+function CheckcorrectAnswer(){
+
+}
+
+function NextquestionIncrment() {
+
+}
+function CorrectAnswer() {
+
+}
+
+function WorngAnswer() {
+
+}
+
+function MusicQuestions() {
+
+}
+
+function MoviesQuestions() {
+
+}
+
+function GeneralKnowlageQuestions() {
+
+}
+
+function Highscore() {
+
+}
+
+function TotalErned() {
+
+}
