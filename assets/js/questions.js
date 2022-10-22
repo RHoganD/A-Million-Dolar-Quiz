90af8004467ca5950abc9bab63a5c906f84aabd7
RHoganD <richpatric@hotmail.com>
2022-10-22 21:52:50 +0000

    tyding up the code

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,7 +1,8 @@
 let questions = [{
+
     numb: 1,
     question: "In the UK, the abbreviation NHS stands for National what Service?",
-    answer: "B.Health",
+    answer: "B. Health",
     options: [
         "A. Humanity",
         "B. Health",
@@ -10,98 +11,125 @@ let questions = [{
     ]
 },  
 {
-    numb: 1,
+    numb: 2,
     question: "Which Disney character famously leaves a glass slipper behind at a royal ball?",
-    answer: "C.Cinderella",
+    answer: "C. Cinderella",
     options: [
         "A. Pocahontas",
         "B. Sleeping Beauty",
         "C. Cinderella",
         "D. Elsa",
-    ]
+             ]
 },  
 {
     numb: 3,
     question: "Who was the youngest British Prime Minister?",
-    answer: "B.William Pitt the Yonger",
+    answer: "B. William Pitt the Yonger",
     options: [
         "A. David Cameron",
         "B. William Pitt the Yonger",
         "C. Tony Blair",
         "D. William Cavendish",
-    ]
+            ]
 },  
-{
+{ 
+    numb: 4,
     question: "Who won the first ever football World Cup?",
-choice1: "Uruguay" ,
-choice2: "Brazil",
-choice3: "Portugal",
-choice4: "Sweden",
-answer: 1
+    answer: "A. Uruguay" ,
+    options: [
+        "A. Uruguay" ,
+        "B. Brazil",
+        "C. Portugal",
+        "D. Sweden",
+            ]
 },
 {  
+    numb: 5,
     question: "What was the only painting sold by Vincent van Gogh during his lifetime?",
-choice1: "Sunflowers",
-choice2: "The Starry Night",
-choice3: "The Red Vineyard",
-choice4: "The Yellow House",
-answer: 3
+    answer: "C. The Red Vineyard",
+    options: [
+        "A. Sunflowers",
+        "B. The Starry Night",
+        "C. The Red Vineyard",
+        "D. The Yellow House",
+          ]
 },  
-{
+{  
+    numb: 6,
     question: "How many million miles away is the sun from the Earth?",
-choice1: "93 million",
-choice2: "12 million",
-choice3: "136 million",
-choice4: "200 million",
-answer: 1
+    answer:  "A. 93 million",
+    options: [
+        "A. 93 million",
+        "B. 12 million",
+        "C. 136 million",
+        "D. 200 million",
+            ]
 },  
 {
+    numb: 7,
     question: "Which singer has a tattoo of Heinz tomato ketchup on his arm?",
-choice1: "Lewis Capaldi",
-choice2: "Martin Kemp",
-choice3: "Tom Jones",
-choice4: "Ed Sheeran",
-answer: 4
+    answer:  "D. Ed Sheeran",
+    options: [
+        "A. Lewis Capaldi",
+        "B. Martin Kemp",
+        "C. Tom Jones",
+        "D. Ed Sheeran",
+           ]
  },
  {
+    numb: 8,
     question: "Duroc, Berkshire and Mangalica are breeds of which farmyard animal?",
-choice1: "Chicken",
-choice2: "Cow",
-choice3: "Pig",
-choice4: "Sheep",
-answer: 3
+    answer:  "C. Pig",
+    options: [
+         "A. Chicken",
+         "B. Cow",
+         "C. Pig",
+         "D. Sheep",
+            ]
  }, 
  { 
+    numb: 9,
     question: "Which city will host the 2028 Olympic Games?",
-choice1: "Beijing",
-choice2: "Paris",
-choice3: "Tokyo",
-choice4: "Los Angeles",
-answer: 4
+    answer:  "D. Los Angeles",
+    options: [
+        "A. Beijing",
+        "B. Paris",
+        "C. Tokyo",
+        "D. Los Angeles",
+            ]
 },
 {  
+    numb: 10,
     question: "In the UK, the abbreviation NHS stands for National what Service?",
-choice1: "Humanity",
-choice2: "Health",
-choice3: "Honour",
-choice4: "Household",
-answer: 2
-},
+    answer: "B. Health",
+    options: [
+        "A. Humanity",
+        "B. Health",
+        "C. Honour",
+        "D. Household",
+            ]
+ },
 {  
+    numb: 11,
     question: "What is the most sold flavour of Walker’s crisps?",
-choice1: "Cheese and Onion",
-choice2: "Salted and Vinegar",
-choice3: "Prawn Cocktail",
-choice4: "Ready Salted",
-answer: 1
+    answer: "A. Cheese and Onion",
+    options: [
+        "A. Cheese and Onion",
+        "B. Salted and Vinegar",
+        "C. Prawn Cocktail",
+        "D. Ready Salted",
+           ]
  },
  {  
+    numb: 12,
     question: "What colour were the Pyramids of Giza originally?",
-choice1: "Orange",
-choice2: "Blue",
-choice3: "Pink",
-choice4: "White",
-answer: 4
+    answer: "D. White",
+    options: [
+         "A. Orange",
+         "B. Blue",
+         "C. Pink",
+         "D. White",
+          ]
  },  
 
  ]
